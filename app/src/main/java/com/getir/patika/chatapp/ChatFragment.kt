af7d51311ebdb56086f07fa128e1ee6bee5e96e2
f-arslan <buildsmstff@gmail.com>
2024-03-29 23:08:59 +0300

    feat: base fragment init

---
+++ app/src/main/java/com/getir/patika/chatapp/ChatFragment.kt
@@ -0,0 +1,14 @@
+package com.getir.patika.chatapp
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.getir.patika.chatapp.databinding.FragmentChatBinding
+
+class ChatFragment : BaseFragment<FragmentChatBinding>() {
+    override fun getBinding(inflater: LayoutInflater, container: ViewGroup?): FragmentChatBinding =
+        FragmentChatBinding.inflate(inflater, container, false)
+
+    override fun initializeViews() {
+
+    }
+}
--- app/src/main/java/com/getir/patika/chatapp/MainActivity.kt
+++ app/src/main/java/com/getir/patika/chatapp/MainActivity.kt
@@ -5,12 +5,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.getir.patika.chatapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        handleInsets()
+    }
+
+    fun handleInsets() {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
