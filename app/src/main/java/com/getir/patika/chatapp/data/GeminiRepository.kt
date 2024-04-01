b3f1ce2f3496cab654e3c0eb6752c7fcda4091b2
f-arslan <buildsmstff@gmail.com>
2024-04-01 22:02:03 +0300

    refactor

--- app/src/main/java/com/getir/patika/chatapp/data/GeminiRepository.kt
+++ app/src/main/java/com/getir/patika/chatapp/data/GeminiRepository.kt
@@ -2,7 +2,6 @@ package com.getir.patika.chatapp.data
 
 import com.google.ai.client.generativeai.type.Content
 
-
 interface GeminiRepository {
     suspend fun getResponse(history: List<Content>, message: String): Result<String>
 }
